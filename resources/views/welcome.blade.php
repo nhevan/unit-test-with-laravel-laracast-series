57b6d4b7e25ce8797d6ef46d6d46e339d60eb78b
NH Evan <nhevan@gmail.com>
2017-05-05 21:52:42 +0600

    setup laravel dusk successfully.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Testing Laravel
                 </div>
 
                 <div class="links">
